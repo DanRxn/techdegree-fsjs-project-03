d69885fb4845bdc1a5c9a81fe5b802257c097def
Dan Robertson <dan@productintuition.com>
2018-02-10 11:26:34 -0800

    Linked local jQuery and page.js scripts

---
+++ js/page.js
@@ -0,0 +1,2 @@
+const h1Content = $('h1').text();
+console.log( ` I know that "${h1Content}" is the h1 of this page, which proves that both 'js/jquery-3.3.1.min.js' and 'js/page.js' have loaded in the page! ` );
