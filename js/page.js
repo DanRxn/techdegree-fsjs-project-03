e6b1e677fdd0b9c243ea481c2cb1ec63760afbfa
Dan Robertson <dan@productintuition.com>
2018-02-24 08:37:57 -0800

    Dynamic functionality for —  • Job Role section of the form • Payment Info section of the form

--- js/page.js
+++ js/page.js
@@ -1,6 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 
+	const activitiesCheckboxes = document.querySelectorAll('.activities label input');
+
+	// # Set focus on the first text field
+		// Done in index.html, since it's not JS and is desired behavior regardless of JS enablement
+
+	// # ”Job Role” section of the form:
+		// A text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.
+	document.querySelector('#other-title').setAttribute('type', 'hidden');
+	document.querySelector('#title').addEventListener('change', () => {
+		const title = document.querySelector('#title').value;
+		switch(title) {
+			case "other":
+				document.querySelector('#other-title').setAttribute('type', 'text');
+				break;
+			default:
+			document.querySelector('#other-title').setAttribute('type', 'hidden');
+		}
+	});
+		// Give the field an id of “other-title,” and add the placeholder text of "Your Job Role" to the field.
+			// 👆 Done in HTML
+
+	// Adjust the color options when design is chosen
 	document.querySelector('#design').addEventListener('change', () => {
 		const theme = $('#design').val();
 		for (i = 0; i < $('#color option').length; i += 1) {
@@ -25,6 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		}		
 	});
 
+	// Auto-select the design, if a color option is chosen first
 	document.querySelector('#color').addEventListener('change', () => {
 		const implicitTheme = $('#color option:selected').parent().attr('id');
 		switch(implicitTheme) {
@@ -38,5 +61,118 @@ document.addEventListener('DOMContentLoaded', () => {
 				console.log(`Error: Something when wrong when selecting a color (implicitTheme = ${implicitTheme}`);
 		}
 	});
+
+
+
+
+	// # ”Register for Activities” section of the form:
+
+		// Disable conflicting events
+
+			// CREATE array of objects from checkboxes on page
+	const arrayifyCheckboxs = (checkboxesHtml) => {
+		const parseActivity = (activity, i) => {
+			const activityText = activity.textContent;
+			const thisIndex = i;
+			const thisTitle = activityText.substr(0, activity.indexOf('—'));
+			const thisDateTime = activityText.substr(activityText.indexOf('–'), activityText.indexOf(','));
+			const thisPriceText = activityText.substr()
+			const thisPrice = parseInt(thisPriceText);
+
+			const thisActivity = {
+				index: thisIndex,
+				title: thisTitle,
+				dateTimes: thisDateTimes,
+				price: thisPrice
+			}
+			return thisActivity;
+		}
+
+
+		// todo
+	}
+			// UPDATE array of checkboxes with display state
+	const updateArrayDisplayFlags = () => {
+		// todo
+	}
+			// 
+		
+		// Total the cost of all events
+
+
+	// # Payment Info section of the form
+
+		// Display payment sections based on the payment option chosen 
+		// in the select menu
+
+	const hideOtherPaymentMethods = (selectedMethod) => {
+		switch(selectedMethod) {
+			case "credit_card":
+				document.querySelector('#credit-card').style.display = '';
+				document.querySelector('#paypal').style.display = 'none';
+				document.querySelector('#bitcoin').style.display = 'none';
+				break;
+			case "paypal":
+			document.querySelector('#credit-card').style.display = 'none';
+			document.querySelector('#paypal').style.display = '';
+			document.querySelector('#bitcoin').style.display = 'none';
+				break;
+			case "bitcoin":
+			document.querySelector('#credit-card').style.display = 'none';
+			document.querySelector('#paypal').style.display = 'none';
+			document.querySelector('#bitcoin').style.display = '';
+				break;
+			default:
+				console.log(`Error: Something went wrong when selecting payment method (selectedMethod = ${selectedMethod}) `);
+		}
+	}
+
+	const setDefaultPayment = () => {
+		const defaultMethod = 'credit_card';
+		document.querySelector('#payment').value = defaultMethod;
+		hideOtherPaymentMethods(defaultMethod);
+	}
+
+
+		// The "Credit Card" payment option should be selected by default, 
+		// display the #credit-card div, and hide the "Paypal" and "Bitcoin information.
+	setDefaultPayment();
+	
+		// When a user selects the "PayPal" payment option, the Paypal information 
+		// should display, and the credit card and “Bitcoin” information should be hidden.
+		
+		// AND/OR
+
+		// When a user selects the "Bitcoin" payment option, the Bitcoin information 
+		// should display, and the credit card and “PayPal” information should be hidden.
+	document.querySelector('#payment').addEventListener("change", () => {
+		const selectedMethod = event.target.value;
+		hideOtherPaymentMethods(selectedMethod);
+	});
+
+
+
+
+
+
+
+
+
+		// SCRATCH: todo DELETE THIS
+	// When the group of checkboxes change state, 
+	// document.querySelector('.activities').addEventListener('change', () => {
+		
+
+		// Enable all checkboxes
+		// for (i = 0; i < activitiesCheckboxes.length; i += 1) {
+		// 	activitiesCheckboxes[i].disabled = false;
+		// }
+		// Loop through each
+	// 	for
+
+	// });
+		
+		// Loop through each
+			// If a checked one conflicts with any others, disable the others
 
 });
